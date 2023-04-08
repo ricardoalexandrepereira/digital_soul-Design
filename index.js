e706be02626cf6d413e75cd940ec8586d6b3fa38
Ricardo Pereira <ricardo_certe@hotmail.com>
2023-04-08 18:13:41 +0100

    add express

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require("path")
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/', (req, res)=> {
+  res.render('index')
+});
+/* app.get('/about', (req, res)=> {
+  res.render('about')
+}); */
+
+app.listen(port, ()=> console.log(`servidor na porta http://localhost:${port}`))
